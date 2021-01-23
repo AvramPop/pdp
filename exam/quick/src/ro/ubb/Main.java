040395b08abd802d30e50d81d522c4e730e78e98
AvramPop <daniel.avram.pop@gmail.com>
2021-01-23 18:41:25 +0200

    quick

--- exam/quick/src/ro/ubb/Main.java
+++ exam/quick/src/ro/ubb/Main.java
@@ -1,31 +1,61 @@
 package ro.ubb;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+  private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 
-    public static void main(String[] args) {
-        List<Integer> array = Arrays.asList(4, 3, 2, 1);
-        quicksort(array, 0, array.size());
-        array.forEach(s -> System.out.print(s + " "));
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+    List<Integer> array = Arrays.asList(9, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    quicksort(array, 0, array.size() - 1);
+    array.forEach(s -> System.out.print(s + " "));
+    EXECUTOR_SERVICE.shutdown();
+    EXECUTOR_SERVICE.awaitTermination(10, TimeUnit.MINUTES);
+  }
+
+  private static Object quicksort(List<Integer> array, int start, int end) throws ExecutionException, InterruptedException {
+    if(start < end) {
+      int p = partition(array, start, end);
+      List<Integer> copy1 = new ArrayList<>(array);
+      List<Integer> copy2 = new ArrayList<>(array);
+      Future<Object> result1 = EXECUTOR_SERVICE.submit(() -> quicksort(copy1, start, p - 1));
+      Future<Object> result2 = EXECUTOR_SERVICE.submit(() -> quicksort(copy2, p + 1, end));
+      result1.get();
+      result2.get();
+      for(int i = start; i <= p - 1; i++) {
+        array.set(i, copy1.get(i));
+      }
+      for(int i = p + 1; i < end; i++) {
+        array.set(i, copy2.get(i));
+      }
+    }
+    return null;
+  }
+
+  private static int partition(List<Integer> array, int start, int end) {
+    int pivot = array.get(end);
+    int i = start - 1;
+    for(int j = start; j < end; j++) {
+      if(array.get(j) < pivot) {
+        i++;
+
+        int temp = array.get(i);
+        array.set(i, array.get(j));
+        array.set(j, temp);
+      }
     }
 
-    private static void quicksort(List<Integer> array, int start, int end) {
-        if (start < end) {
-            int p = partition(array, start, end);
-            quicksort(array, start, p);
-            quicksort(array, p, end);
-        }
-    }
+    int temp = array.get(i + 1);
+    array.set(i + 1, array.get(end));
+    array.set(end, temp);
 
-    private static int partition(List<Integer> array, int start, int end) {
-        int pivot = array.get(end);
-        int i = start - 1;
-        for(int j = start; j < end; j++) {
-            if (array.get(j)) {
-                i++;
-            }
-        }
-    }
+    return i + 1;
+  }
 }
