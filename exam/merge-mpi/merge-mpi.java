a019e8c5bc4f89a70b91a246fb2620e5941e5842
AvramPop <daniel.avram.pop@gmail.com>
2021-01-24 16:42:10 +0200

    zoli e tare

--- exam/merge-mpi/merge-mpi.java
+++ exam/merge-mpi/merge-mpi.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import mpi.MPI;
+import mpi.Status;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,22 +9,27 @@ import java.util.List;
 
 public class Main {
 
+    private static void killAll(int numberOfProcess) {
+        for (int i = 1; i < numberOfProcess; ++i) {
+            MPI.COMM_WORLD.Send(new int[]{0}, 0, 1, MPI.INT, i, 2);
+        }
+    }
+
     private static void master(List<Integer> array, int numberOfProcesses) {
         List<Integer> solution = mergeSortUtil(array, 0, numberOfProcesses);
         System.out.println("--------------");
         solution.forEach(s -> System.out.print(s + " "));
+        killAll(numberOfProcesses);
     }
 
     private static List<Integer> mergeSortUtil(List<Integer> array, int me, int numberOfProcesses) {
-//        System.out.println("received to sort util: " + array.toString());
         if (array.size() <= 1) return array;
         List<Integer> result1, result2;
-        if (numberOfProcesses >= 2) {
-            int child = me + numberOfProcesses / 2;
+        int child = me + numberOfProcesses / 2;
+        if (numberOfProcesses >= 2 && child < numberOfProcesses) {
             List<Integer> toSend = new ArrayList<>(array.subList(0, array.size() / 2));
-//            System.out.println("master = parent is: " + me + " child is " + child + " [] " + toSend.toString());
+            MPI.COMM_WORLD.Send(new int[]{1}, 0, 1, MPI.INT, child, 2);
             MPI.COMM_WORLD.Send(new Object[]{toSend}, 0, 1, MPI.OBJECT, child, 0);
-//            System.out.println("master = parent is " + me + " local sort " + "[]" + array.subList(array.size() / 2, array.size()).toString());
             result2 = mergeSortUtil(array.subList(array.size() / 2, array.size()), me, numberOfProcesses / 2);
             Object[] receivedData = new Object[1];
             MPI.COMM_WORLD.Recv(receivedData, 0, 1, MPI.OBJECT, child, 0);
@@ -52,31 +58,21 @@ public class Main {
         return result;
     }
 
-    private static void worker(int n, int me, int numberOfProcesses) {
-        int base = 0;
-        int parent = -1;
-        int offset = me;
-        while(offset > 0) {
-            parent = base;
-            int mid = numberOfProcesses / 2;
-            if(offset < mid) {
-                n = n / 2;
-                numberOfProcesses = numberOfProcesses / 2;
-            } else {
-                offset = offset - mid;
-                n = n / 2 + n % 2;
-                numberOfProcesses = numberOfProcesses / 2 + numberOfProcesses % 2;
-                base += mid;
+    private static void worker(int me, int numberOfProcesses) {
+        while (true) {
+            int[] alive = new int[1];
+            MPI.COMM_WORLD.Recv(alive, 0, 1, MPI.INT, MPI.ANY_SOURCE, 2);
+            if (alive[0] == 0) {
+                break;
             }
+            Object[] receivedData = new Object[1];
+            Status status = MPI.COMM_WORLD.Recv(receivedData, 0, 1, MPI.OBJECT, MPI.ANY_SOURCE, 0);
+            int parent = status.source;
+            List<Integer> array = (List<Integer>) receivedData[0];
+            array = mergeSortUtil(array, me, numberOfProcesses);
+
+            MPI.COMM_WORLD.Send(new Object[]{array}, 0, 1, MPI.OBJECT, parent, 0);
         }
-
-        Object[] receivedData = new Object[1];
-        MPI.COMM_WORLD.Recv(receivedData, 0, 1, MPI.OBJECT, parent, 0);
-        List<Integer> array = (List<Integer>) receivedData[0];
-//        System.out.println("child = child is " + me + " parent is " + parent + " [] " + array.toString());
-        array = mergeSortUtil(array, me, numberOfProcesses);
-
-        MPI.COMM_WORLD.Send(new Object[]{array}, 0, 1, MPI.OBJECT, parent, 0);
     }
 
     public static void main(String[] args) {
@@ -87,7 +83,7 @@ public class Main {
         if (selfRank == 0) {
             master(array, numberOfProcesses);
         } else {
-            worker(array.size(), selfRank, numberOfProcesses);
+            worker(selfRank, numberOfProcesses);
         }
         MPI.Finalize();
     }
